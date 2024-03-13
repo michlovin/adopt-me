2020c9552db08af607a43aa242b783ccfd7f3dd7
Michlovin <michb5@hotmail.com>
2024-03-13 12:14:05 -0600

    get pet by id

--- src/services/petService.ts
+++ src/services/petService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { Pet } from "../models/pet";
 
-const apiURL = process.env.REACT_APP_API_URL || "";
+const apiURL = process.env.REACT_APP_API_URL + "pets" || "";
+//if you give me a search term it will look, if not it just lists all the pets as an array of pets
 
 export const getPets = async (searchTerm?: string): Promise<Pet[]> => {
   let response;
@@ -11,4 +12,9 @@ export const getPets = async (searchTerm?: string): Promise<Pet[]> => {
     response = await axios.get(apiURL);
   }
   return response.data as Pet[];
+};
+
+export const getPetById = async (id: number): Promise<Pet> => {
+  const response = await axios.get(`${apiURL}/${id}`);
+  return response.data as Pet;
 };
