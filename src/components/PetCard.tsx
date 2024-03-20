811cee80cbd8e2fb4cb06fca1c63d3fa508c3b7b
Shell <bjorndalenm@gmail.com>
2024-03-20 20:25:09 +0000

    feat: wip

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Pet } from "../models/Pet";
 import "./css/petcard.css";
 
@@ -11,8 +12,12 @@ interface PetCardProps {
 
 export function PetCard(props: PetCardProps) {
   return (
-    <Card>
-      <Card.Img variant="top" src={`img/${props.pet.image}`}></Card.Img>
+    <Card className="space-below">
+      <Card.Img
+        variant="top"
+        src={`img/${props.pet.image}`}
+        className="card-img"
+      ></Card.Img>
 
       <Card.Body>
         <Card.Title>
@@ -22,10 +27,14 @@ export function PetCard(props: PetCardProps) {
         </Card.Title>
         {props.pet.description}
       </Card.Body>
-      <Card.Footer>
+      <Card.Footer className="space-below">
         <button className="btn btn-primary">Book A Viewing</button>
-        <button className="btn btn-primary">Adopt Me</button>
-        <button className="btn btn-primary">Details</button>
+        <Link
+          to={`/details/${props.pet.id}`}
+          className="btn btn-secondary ml-2"
+        >
+          {props.pet.name}'s Details
+        </Link>
       </Card.Footer>
     </Card>
   );
