4743b14a30e9d2d53c2a661910c105ecdb554ef5
Michlovin <michb5@hotmail.com>
2024-03-26 22:25:03 -0600

    hotzontal card

---
+++ src/components/HorizontalCard.tsx
@@ -0,0 +1,21 @@
+import Image from "react-bootstrap/Image";
+
+export function HorizontialCard() {
+  return (
+    <div className="horizontialcardcontainer">
+      <div className="card">
+        <Image></Image>
+        <div className="title">Title</div>
+        <div className="subtext">Subtitle</div>
+        <div className="textfield">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
