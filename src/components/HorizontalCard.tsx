170659e64360363ead5830dbf036bdf0b3faf5f1
Michlovin <michb5@hotmail.com>
2024-04-01 21:16:22 -0600

    horzontial card position

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -1,21 +1,28 @@
+import { Col, Row } from "react-bootstrap";
 import Animal from "./animal2.jpg";
 
 export function HorizontialCard() {
   return (
-    <div className="horizontialcardcontainer">
-      <div className="card">
-        <img src="{Animal}" alt="image"></img>
-        <div className="title">Title</div>
-        <div className="subtext">Subtitle</div>
-        <div className="textfield">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.{" "}
+    <Row>
+      <Col>
+        <div className="image-container">
+          <img src="{Animal}" alt="image"></img>
         </div>
-      </div>
-    </div>
+      </Col>
+      <Col>
+        <div className="content">
+          <div className="title">Title</div>
+          <div className="subtext">Subtitle</div>
+          <div className="textfield">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{" "}
+          </div>
+        </div>
+      </Col>
+    </Row>
   );
 }
