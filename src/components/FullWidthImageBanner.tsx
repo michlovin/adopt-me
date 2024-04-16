905ecfeae662df37c94647b9385c012dd4bdc5b1
Michlovin <michb5@hotmail.com>
2024-04-15 23:24:51 -0600

    banner

--- src/components/FullWidthImageBanner.tsx
+++ src/components/FullWidthImageBanner.tsx
@@ -1,9 +1,17 @@
 import banner from "./assets/pawsbanner.jpeg";
+import "../components/css/fullwidthbanner.css";
 
 export function FullWidthImageBanner() {
   return (
-    <div className="text-card-wrapper">
-      <img src={banner} alt="image"></img>
-    </div>
+    <>
+      <div id="container">
+        <div className="scroll">
+          <div className="item">
+            <img src={banner} alt="image"></img>
+          </div>
+        </div>
+        <div className="fade"></div>
+      </div>
+    </>
   );
 }
