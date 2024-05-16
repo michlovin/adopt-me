0974acbca955f43a928d3e0d639d1e00c2440f56
Michlovin <michb5@hotmail.com>
2024-05-16 10:04:25 -0600

    uid pushed to db if user is signed in

--- src/services/intakeService.ts
+++ src/services/intakeService.ts
@@ -1,5 +1,5 @@
 import { addDoc, collection } from "firebase/firestore";
-import { database } from "../FireBase/FirebaseProvider";
+import { auth, database } from "../FireBase/FirebaseProvider";
 import { SurrenderedPet } from "../models/SurrenderedPet";
 
 const apiURL = process.env.REACT_APP_API_URL + "intake" || "";
@@ -12,6 +12,7 @@ export const postIntake = async (
     let collectionRef = collection(database, "SurrenderedPets");
     await addDoc(collectionRef, {
       ...intakeValues,
+      userId: auth?.currentUser?.uid,
     });
     console.log("Submitted surrendered pet successfully!");
   } catch (ex) {
